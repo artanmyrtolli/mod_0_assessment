289699efed902e72279eda27104e659d8af9853a
Artan Myrtolli <artanmyrtolli@gmail.com>
2021-11-09 23:22:27 -0500

    Add house to modelB

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  color: 'white',
+  style: 'colonial',
+  floors: 3,
+  yearBuilt: 1928,
+  needsWork: true,
+  isForSale: false,
+  roomsFirstFloor: ['Kitchen', 'Dining', 'Living'],
+  roomsSecondFloor: ['Bedroom', 'Bedroom', 'Bathroom']
+}
