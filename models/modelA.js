67484974d54487f04aaba4363774c27164e26a5c
Artan Myrtolli <artanmyrtolli@gmail.com>
2021-11-09 23:14:21 -0500

    Add laptop to modelA

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptop = {
+  brand: 'Asus',
+  model: 'Q537',
+  screenSizeInInch: 15,
+  weightInLbs: 14,
+  isGamingLaptop: true,
+  isExpensive: true,
+  networkAdapters: ['Bluetooth', 'Intel(R) Wi-Fi 6 AX201 160MHz', 'WAN Miniport (IP)'],
+  HumanInterfaceDevices: ['Asus Precision Touchpad', 'HID PCI Minidriver for ISS', 'GPIO Laptop or Slate Indicator Driver']
+}
