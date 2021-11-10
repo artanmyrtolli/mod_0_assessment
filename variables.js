32b72abad2b815ac2b07263ab1b30062340a6886
Artan Myrtolli <artanmyrtolli@gmail.com>
2021-11-09 23:00:13 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var animal = "Cheetah";
+var isCuddly = true;
+
+animal = "Bear";
+
+var num8 = 4;
+var num3 = 12;
+
+var quotient = num3 / num8;
+
+var favoriteFilms = ['The Bourne Identiy', 'Rocky', 'Little Miss Sunshine'];
+
+favoriteFilms.shift();
+console.log(animal.length);
